6447c94f777cfd7bd14a1dbd182d62100e4a69b5
nomadhuns <kimalss22@gmail.com>
2023-06-28 16:12:49 +0900

    회원가입 기능 Rest로 변경

--- src/main/java/com/example/kakaoshop/user/UserRequest.java
+++ src/main/java/com/example/kakaoshop/user/UserRequest.java
@@ -1,4 +1,25 @@
 package com.example.kakaoshop.user;
 
+import lombok.*;
+import java.util.Collections;
+
 public class UserRequest {
+
+    @Getter
+    @Setter
+    public static class JoinDTO {
+
+        private String email;
+        private String password;
+        private String username;
+
+        public User toEntity() {
+            return User.builder()
+                    .email(email)
+                    .password(password)
+                    .username(username)
+                    .roles(Collections.singletonList("ROLE_USER"))
+                    .build();
+        }
+    }
 }
--- src/main/java/com/example/kakaoshop/user/UserRestController.java
+++ src/main/java/com/example/kakaoshop/user/UserRestController.java
@@ -1,4 +1,30 @@
 package com.example.kakaoshop.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequiredArgsConstructor
 public class UserRestController {
+    private final UserJPARepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO joinDTO) {
+        User user = joinDTO.toEntity();
+
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            return ResponseEntity.ok("fail");
+        }
+
+        return ResponseEntity.ok("ok");
+    }
 }
